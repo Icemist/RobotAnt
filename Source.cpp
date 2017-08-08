77868a7a80d873df9e4f979c78505c1f5f2f3a31
Icemist <icemist@mail.ru>
2017-08-09 00:33:50 +0300

    changed a testing code

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <unistd.h>
 #include "Ant.h"
 
 int main() {
 	Ant ant;
-	ant.action(LEG_NAME::RightBack, SEGMENT_NAME::Coxa, 90, Segment::MAX_SPEED);
-	ant.action(LEG_NAME::RightBack, SEGMENT_NAME::Tabia, 30, Segment::MAX_SPEED / 2);
-	//need wait! and to do next 
+        //for(int i = 0; i < 1; ++i){
+            ant.action(LEG_NAME::RightFront, SEGMENT_NAME::Coxa, 70, Segment::MAX_SPEED);
+            sleep(1);
+            ant.action(LEG_NAME::RightFront, SEGMENT_NAME::Trochanter, 70, Segment::MAX_SPEED);
+            sleep(1);
+            ant.action(LEG_NAME::RightFront, SEGMENT_NAME::Femur, 70, Segment::MAX_SPEED);
+            sleep(1);
+            ant.action(LEG_NAME::RightFront, SEGMENT_NAME::Tabia, 70, Segment::MAX_SPEED);
+            sleep(1);
+            ant.action(LEG_NAME::RightFront, SEGMENT_NAME::Tarsus, 70, Segment::MAX_SPEED);        
+            sleep(1);
+            ant.action(LEG_NAME::RightFront, SEGMENT_NAME::Coxa, 120, Segment::MAX_SPEED);
+            sleep(1);
+            ant.action(LEG_NAME::RightFront, SEGMENT_NAME::Trochanter, 120, Segment::MAX_SPEED);
+            sleep(1);
+            ant.action(LEG_NAME::RightFront, SEGMENT_NAME::Femur, 120, Segment::MAX_SPEED);
+            sleep(1);
+            ant.action(LEG_NAME::RightFront, SEGMENT_NAME::Tabia, 120, Segment::MAX_SPEED);
+            sleep(1);
+            ant.action(LEG_NAME::RightFront, SEGMENT_NAME::Tarsus, 120, Segment::MAX_SPEED);        
+            sleep(1);
+        //}
 	return 0;
 }
