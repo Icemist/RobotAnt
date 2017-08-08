0cb8067a999e969b03a5f303de34e7abfeeff44c
Icemist <icemist@mail.ru>
2017-08-08 21:29:26 +0300

    added first example a robot ant controlling

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+#include "Ant.h"
 
 int main() {
-
+	Ant ant;
+	ant.action(LEG_NAME::RightBack, SEGMENT_NAME::Coxa, 90, Segment::MAX_SPEED);
+	ant.action(LEG_NAME::RightBack, SEGMENT_NAME::Tabia, 30, Segment::MAX_SPEED / 2);
+	//need wait! and to do next 
 	return 0;
 }
